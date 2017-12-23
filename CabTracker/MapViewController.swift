1eb16b7dcc7e854945145afe94f6e23a8ef63d67
Sai Pathuri <saipathuri@gmail.com>
2017-12-23 15:16:21 +0530

    initial commit of app

---
+++ CabTracker/MapViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MapViewController.swift
+//  CabTracker
+//
+//  Created by Sai Pathuri on 12/23/17.
+//  Copyright © 2017 Sai Pathuri. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class MapViewController: UIViewController {
+    let UTD_CENTER_LAT = 32.9859896
+    let UTD_CENTER_LONG = -96.7517943
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func loadView() {
+        // Create a GMSCameraPosition
+        let camera = GMSCameraPosition.camera(withLatitude: UTD_CENTER_LAT, longitude: UTD_CENTER_LONG, zoom: 16.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+        if let mylocation = mapView.myLocation {
+            print("User's location: \(mylocation)")
+        } else {
+            print("User's location is unknown")
+        }
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+//        let marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+//        marker.title = "Univeristy of "
+//        marker.snippet = "Australia"
+//        marker.map = mapView
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '11.1.2'
+use_frameworks!
+
+target 'CabTracker' do
+  pod 'GoogleMaps'
+  pod 'Alamofire', '~> 4.5'
+end
